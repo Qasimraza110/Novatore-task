f9ab80ba6f15173eccdb87a5849b04174db79b35
Qasim Raza <sh.qasim.raza110@gmail.com>
2025-08-29 12:29:34 +0500

    Update store.js

--- react basic/basic_ecomm/src/redux/store.js
+++ react basic/basic_ecomm/src/redux/store.js
@@ -1,8 +1,34 @@
 import { configureStore } from "@reduxjs/toolkit";
-import cartReducer from "./cart/cartSlice";     
+import cartReducer from "./cart/cartSlice";
+
+const loadCart = () => {
+  try {
+    const serializedCart = localStorage.getItem("cart");
+    if (serializedCart === null) {
+      return undefined;
+    }
+    return { cart: { items: JSON.parse(serializedCart) } };
+  } catch (e) {
+    return undefined;
+  }
+};
+
+const saveCart = (state) => {
+  try {
+    localStorage.setItem("cart", JSON.stringify(state.cart.items));
+  } catch (e) {
+  }
+};
+
+const preloadedState = loadCart();
 
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
   },
+  preloadedState,
+});
+
+store.subscribe(() => {
+  saveCart(store.getState());
 });
